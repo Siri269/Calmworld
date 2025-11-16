27c92e570a9d268f5dccced3a3db042f5d15e063
toastandeggs473 <craigdixonwilliams@gmail.com>
2025-11-15 17:31:30 -0800

    Update breathein.js

--- breathein.js
+++ breathein.js
@@ -6,4 +6,8 @@ document.getElementById("changeScreenButton").addEventListener("click", function
   setTimeout(navigateToNewScreen, 4000);
 });
 
-window.onload = setTimeout(navigateToNewScreen, 4000);
+function change(){
+  setTimeout(navigateToNewScreen, 4000);
+}
+
+window.onload = change;
